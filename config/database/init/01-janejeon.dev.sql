7658950c9323c7078d9b1bff0fdc2362f9fa0b9f
Jane Jeon <me@janejeon.dev>
2021-10-30 22:32:31 -0400

    fix ghost!

--- config/database/init/01-janejeon.dev.sql
+++ config/database/init/01-janejeon.dev.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS devblog;
 
-CREATE USER IF NOT EXISTS 'ghost-devblog'@'localhost' IDENTIFIED WITH mysql_native_password BY 'ghost-devblog-password';
-GRANT ALL PRIVILEGES ON devblog.* TO 'ghost-devblog'@'localhost';
+CREATE USER IF NOT EXISTS 'ghost-devblog'@'%' IDENTIFIED WITH mysql_native_password BY 'ghost-devblog-password';
+GRANT ALL PRIVILEGES ON devblog.* TO 'ghost-devblog'@'%';
