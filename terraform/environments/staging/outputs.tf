e8fd8a4403a70068f7556dc961fdf5dfb59907b2
Jane Jeon <git@janejeon.com>
2023-02-17 19:39:31 +0700

    add outputs

--- terraform/environments/staging/outputs.tf
+++ terraform/environments/staging/outputs.tf
@@ -1,1 +1,29 @@
+output "managed_by_terraform_total_resources_count" {
+  value       = module.tag_managed_by_terraform.total_resource_count
+  description = "Total count of resources that are managed by terraform."
+}
 
+output "managed_by_terraform_droplets_count" {
+  value       = module.tag_managed_by_terraform.droplets_count
+  description = "Total count of droplets that are managed by terraform."
+}
+
+output "managed_by_terraform_volumes_count" {
+  value       = module.tag_managed_by_terraform.volumes_count
+  description = "Total count of volumes that are managed by terraform."
+}
+
+output "webserver_total_resources_count" {
+  value       = module.tag_webserver.total_resource_count
+  description = "Total count of webserver resources."
+}
+
+output "webserver_droplets_count" {
+  value       = module.tag_webserver.droplets_count
+  description = "Total count of webserver droplets."
+}
+
+output "webserver_volumes_count" {
+  value       = module.tag_webserver.volumes_count
+  description = "Total count of webserver volumes."
+}
