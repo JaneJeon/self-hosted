4b5cd120098f488f03993d2e2f876581e5ff6c4f
Jane Jeon <git@janejeon.com>
2023-02-18 12:46:50 +0700

    add firewall

--- terraform/environments/staging/main.tf
+++ terraform/environments/staging/main.tf
@@ -13,19 +13,66 @@ provider "digitalocean" {}
 
 resource "digitalocean_project" "project" {
   name        = "Self Hosted"
-  description = "Project containing all of the self hosted stack"
+  description = "Project containing all of the self hosted stack; managed by terraform"
   environment = "Staging"
 }
 
-resource "digitalocean_tag" "tag_webserver" {
+# Simply create duplicate tags across environments, with a different name;
+# this is because we do not want to be sharing resources across environments.
+resource "digitalocean_tag" "webserver" {
   name = "staging-webserver"
 }
 
-resource "digitalocean_tag" "tag_managed_by_terraform" {
+resource "digitalocean_tag" "managed_by_terraform" {
   name = "staging-terraform"
 }
 
-resource "digitalocean_ssh_key" "ssh_key" {
+# Same with the SSH Key; create a different one for isolation.
+resource "digitalocean_ssh_key" "default" {
   name       = "Self-Hosted (Staging) Default SSH Key"
   public_key = var.ssh_public_key
+}
+
+locals {
+  all_ports = "1-65535"
+}
+
+# Same with firewalls; these can't be scoped to a project/environment "naturally" either.
+resource "digitalocean_firewall" "webserver" {
+  name = "staging-webserver-firewall"
+
+  # Apply the firewall by linking to a tag, rather than to individual droplets.
+  tags = [digitalocean_tag.webserver.name]
+
+  # Port 22: Allow SSH
+  inbound_rule {
+    protocol   = "tcp"
+    port_range = "22"
+  }
+
+  # Port 80: Allow HTTP (only from Cloudflare)
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = var.cloudflare_ips
+  }
+
+  # Port 443: Allow HTTPS (only from Cloudflare)
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = var.cloudflare_ips
+  }
+
+  # TCP: Allow all outbound connections
+  outbound_rule {
+    protocol   = "tcp"
+    port_range = local.all_ports
+  }
+
+  # UDP: Allow all outbound connections
+  outbound_rule {
+    protocol   = "udp"
+    port_range = local.all_ports
+  }
 }
--- terraform/environments/staging/variables.tf
+++ terraform/environments/staging/variables.tf
@@ -8,3 +8,8 @@ variable "ssh_public_key" {
   description = "The public key to add to DigitalOcean account and services"
   type        = string
 }
+
+variable "cloudflare_ips" {
+  description = "IP addresses of Cloudflare; see src/get_cloudflare_ips.py"
+  type        = list(string)
+}
