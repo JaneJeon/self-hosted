2ffa60b6a73bd2fa545e26d9dc970d436e57bed4
Jane Jeon <git@janejeon.com>
2023-02-18 14:11:29 +0700

    hardcode it for now

--- terraform/environments/staging/main.tf
+++ terraform/environments/staging/main.tf
@@ -36,6 +36,8 @@ resource "digitalocean_ssh_key" "default" {
 locals {
   all_ports = "1-65535"
   all_ips   = ["0.0.0.0/0", "::/0"]
+  # See src/get_cloudflare_ips.py
+  cloudflare_ips = ["103.21.244.0/22", "103.22.200.0/22", "103.31.4.0/22", "104.16.0.0/13", "104.24.0.0/14", "108.162.192.0/18", "131.0.72.0/22", "141.101.64.0/18", "162.158.0.0/15", "172.64.0.0/13", "173.245.48.0/20", "188.114.96.0/20", "190.93.240.0/20", "197.234.240.0/22", "198.41.128.0/17", "2400:cb00::/32", "2405:8100::/32", "2405:b500::/32", "2606:4700::/32", "2803:f800::/32", "2a06:98c0::/29", "2c0f:f248::/32"]
 }
 
 # Same with firewalls; these can't be scoped to a project/environment "naturally" either.
@@ -56,14 +58,14 @@ resource "digitalocean_firewall" "webserver" {
   inbound_rule {
     protocol         = "tcp"
     port_range       = "80"
-    source_addresses = var.cloudflare_ips
+    source_addresses = local.cloudflare_ips
   }
 
   # Port 443: Allow HTTPS (only from Cloudflare)
   inbound_rule {
     protocol         = "tcp"
     port_range       = "443"
-    source_addresses = var.cloudflare_ips
+    source_addresses = local.cloudflare_ips
   }
 
   # TCP: Allow all outbound connections
