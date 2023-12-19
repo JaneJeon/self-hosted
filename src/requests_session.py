50ba99f086fc59a7ec66995aa021f484147d8af0
Jane Jeon <git@janejeon.com>
2023-12-19 16:46:48 +0000

    fake user agent

--- src/requests_session.py
+++ src/requests_session.py
@@ -1,4 +1,7 @@
 import requests
+from fake_useragent import UserAgent
+
+ua = UserAgent()
 
 
 def _raise_on_status(r: requests.Response, *args, **kwargs) -> None:
@@ -17,5 +20,7 @@ def get_session() -> requests.Session:
 
     # Note: no need to log requests, as urllib3 already does it if we set the logging level to DEBUG
     session.hooks = {"response": [_raise_on_status]}
+    session.timeout = 10
+    session.headers = {"User-Agent": ua.chrome}
 
     return session
