512300e546cf1a2496d587be727fb3cbeb66d325
Jane Jeon <git@janejeon.com>
2024-05-05 18:59:53 +0100

    remove portfolio website

--- services/database/init/02-nariahn.com.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS portfolio;
-
-CREATE USER IF NOT EXISTS 'ghost-portfolio'@'%' IDENTIFIED WITH caching_sha2_password BY 'ghost-portfolio-password';
-GRANT ALL PRIVILEGES ON portfolio.* TO 'ghost-portfolio'@'%';
